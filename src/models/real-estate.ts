d4a685a98e7123a5f9410cd1aeba81e474b89e85
Abdelilah Ajjouqa <abdouajjouqa@gmail.com>
2025-10-22 14:30:22 +0100

    feat: update dependencies and add real estate model #11

---
+++ src/models/real-estate.ts
@@ -0,0 +1,47 @@
+require("mongoose");
+
+const realEstateSchema = new mongoose.Schema({
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    transactionType: { type: String, required: true, enum: ['sale', 'daily rental', 'monthly', 'seasonal'] },
+    price: { type: Number, required: true, min: 0 },
+    availability: { type: Boolean, default: true },
+
+    location: {
+        address: { type: String, required: true },
+        coordinates: { types: Number, required: true, min: -90, max: 90 },
+        longitudd: { type: Number, required: true, min: -180, max: 180 },
+    },
+
+    characteristics: {
+        totalSurface: { type: Number, required: true, min: 0 },
+        roomDimensions: [{ roomName: String, length: Number, width: Number, surface: Number }],
+        numberOfBedRooms: { type: Number, required: true, min: 0 },
+        numberOfBathrooms: { type: Number, required: true, min: 0 },
+        equipment: {
+            wifi: { type: Boolean, default: false },
+            airConditioning: { type: Boolean, default: false },
+            parking: { type: Boolean, default: false },
+            heating: { type: Boolean, default: false },
+            balcony: { type: Boolean, default: false },
+            garden: { type: Boolean, default: false },
+            pool: { type: Boolean, default: false },
+            elevator: { type: Boolean, default: false },
+        },
+        internalRules: {
+            animalsAllowed: { type: Boolean, default: false },
+            smokingAllowed: { type: Boolean, default: false },
+            partiesAllowed: { type: Boolean, default: false }
+        },
+        energyDiagnostics: { type: String }
+    },
+    timestamp: true
+});
+
+realEstateSchema.index({ 'location.coordinates.latitude': 1, 'location.coordinates.longitude': 1 });
+realEstateSchema.index({ transactionType: 1, price: 1 });
+realEstateSchema.index({ availability: 1 });
+
+const realEstate = mongoose.model('RealEstate', realEstateSchema);
+
+module.exports = realEstate;
